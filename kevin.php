f197a620043829db58b13a5648c17c1f12804138
Kevin Gates <zhouwensheng@hihooray.com>
2017-03-09 13:50:27 +0800

    first commit

--- kevin.php
+++ kevin.php
@@ -2,7 +2,7 @@
 $query='vps';
 $useragent = "Opera/9.80 (J2ME/MIDP; Opera Mini/4.2.14912/870; U; id) Presto/2.4.15";
 $ch = curl_init ("");
-curl_setopt ($ch, CURLOPT_URL, "http://www.google.co.uk/search?hl=en&tbo=d&site=&source=hp&q=".$query);
+curl_setopt ($ch, CURLOPT_URL, "https://www.google.co.uk/");
 curl_setopt ($ch, CURLOPT_USERAGENT, $useragent); // set user agent
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
