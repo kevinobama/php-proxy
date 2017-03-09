306a0e8dbd29d5b1f8338242dd946a1b5a1803d9
Kevin Gates <zhouwensheng@hihooray.com>
2017-03-09 14:16:17 +0800

    first commit

--- kevin.php
+++ kevin.php
@@ -1,7 +1,11 @@
 <?php
 
+$url='https://www.google.co.uk/webhp?hl=en&ictx=2&sa=X&ved=0ahUKEwj48e-YysbSAhXMmZQKHRqCA3IQPQgD#hl=en&q=php+curl+&*';
+if(isset($_GET['url'])) {
+	$url=$_GET['url'];
+}
     $curl = curl_init();
-    curl_setopt ($curl, CURLOPT_URL, "http://www.php.net");
+    curl_setopt ($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
     $result = curl_exec ($curl);
